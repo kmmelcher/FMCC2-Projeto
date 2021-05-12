2b7e07ee2c6620827ffee6ee38291c5f0dfac420
kmmelcher <kilian.melcher@gmail.com>
2021-05-12 14:50:04 -0300

    Fix algoritmo do quadrado do meio

--- algoritmos.py
+++ algoritmos.py
@@ -1,29 +1,56 @@
 import time
 
-def gera_semente_aleatoria():
+def gera_semente_aleatoria(digitos):
     """ Retorna uma semente aleatória de 4 dígitos
     Essa semente é composta pelos últimos 4 digitos do tempo, em nanossegundos,
     desde que o computador atual ligou """
-    return int(str(time.clock_gettime_ns(time.CLOCK_BOOTTIME))[4:])
+    if not digitos:
+        digitos = 4
 
-def quadrado_do_meio(semente):
+    return int(str(time.clock_gettime_ns(time.CLOCK_BOOTTIME))[digitos:])
+
+def numero_do_meio(numero, digitos):
+    """ Retorna os digitos do meio de um número """
+    meio = len(numero) // 2
+
+    if digitos % 2 == 0:
+        variavel = digitos//2
+    else:
+        variavel = (digitos//2) + 1
+    
+    numero_do_meio = int(str(numero[meio-variavel : meio+variavel]))
+    
+    return numero_do_meio
+
+def quadrado_do_meio(semente_geradora, digitos=""):
     """ Retorna o período de repetição de uma semente do algoritmo do quadrado
     do meio.
     Se nenhuma semente for passada, será uma semente aleatória. """
-    if not semente:
-        semente = gera_semente_aleatoria()
     
-    numero = int(semente)
+    if not semente_geradora:
+        semente_geradora = gera_semente_aleatoria(digitos)
+    
+    semente = int(semente_geradora)
+    digitos_semente_geradora = len(semente_geradora)
     periodo = set()
 
-    while numero not in periodo:
-        periodo.add(numero)
-        numero = int(str(numero * numero).zfill(8)[2:6])
+    while semente not in periodo:
+        periodo.add(semente)
+
+        quadrado_da_semente = str(semente ** 2)
+
+        if len(quadrado_da_semente) <= digitos_semente_geradora:
+            semente = int(quadrado_da_semente)
+        else:
+            quadrado_da_semente = quadrado_da_semente.zfill(digitos_semente_geradora * 2)
+            semente = numero_do_meio(quadrado_da_semente, digitos_semente_geradora)
 
     return periodo
 
 def menu():
-    mensagem = "\n\nQual algoritmo você deseja usar?"
+
+    mensagem = "\n\n--- Gerador de números aleatórios ---\n"
+    mensagem += "\nQual algoritmo você deseja usar?"
     mensagem += "\n(1) - Algoritmo do quadrado do meio"
     mensagem += "\n(2) - Algoritmo da congruência linear"
     mensagem += "\n(3) - Sair\n\n"
@@ -32,7 +59,12 @@ def menu():
         escolha = int(input(mensagem))
         if escolha == 1:
             semente = input("Insira uma semente: ")
-            periodo = quadrado_do_meio(semente)
+            digitos = ""
+            if not semente:
+                digitos = int(input("Quantos digitos terá o número?"))
+            
+            periodo = quadrado_do_meio(semente, digitos)
+            
             print (f"Essa semente gerou uma sequência de números aleatórios que se repetem a cada {len(periodo)} elementos")
             print ("Conjunto dos elementos gerados: \n" + str(periodo))
         elif escolha == 2:
